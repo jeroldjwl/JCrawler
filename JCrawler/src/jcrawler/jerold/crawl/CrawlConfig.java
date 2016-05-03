51a8c1f1cd577674e4043b29f4fe26edd9802a1d
jeroldjwl <1017794273@qq.com>
2016-05-03 19:30:28 +0800

    update 201605031930

--- JCrawler/src/jcrawler/jerold/crawl/CrawlConfig.java
+++ JCrawler/src/jcrawler/jerold/crawl/CrawlConfig.java
@@ -2,7 +2,9 @@ package jcrawler.jerold.crawl;
 
 public class CrawlConfig {
 	public static final String CRAWL_DOWNLOAD_PATH = "/tmp/crawl/attachments/";
-	protected static final String CRAWL_LIMIT_PATH = "http://www.shyp.gov.cn/";
-	public static final String CRAWL_PATH = "http://www.shyp.gov.cn/";
+	//protected static final String CRAWL_LIMIT_PATH = "http://www.shyp.gov.cn/";
+	protected static final String CRAWL_LIMIT_PATH = "http://zwdt.sh.gov.cn/zwdtSW/index.do";
+	//public static final String CRAWL_PATH = "http://www.shyp.gov.cn/";
+	public static final String CRAWL_PATH = "http://zwdt.sh.gov.cn/zwdtSW/index.do";
 	public static int CRAWL_THREAD_NUM = 6;
 }
